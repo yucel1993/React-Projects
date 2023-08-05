0fcd8c8f4289b1c9197e344536ad996b869c5de9
Burhan <alpagut1993@gmail.com>
2023-08-05 16:46:53 +0200

    gif: added

--- contact App/src/components/ContactForm.jsx
+++ contact App/src/components/ContactForm.jsx
@@ -91,16 +91,15 @@ function ContactForm({ handleSubmit, info, setInfo }) {
             >
               Phone NUMBER
             </label>
-            
+            <input
               className="text-gray-700 appearance-none block w-full bg-gray-200  border border-gray-200 rounded  py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="number"
               placeholder="Enter your Phone Number"
               name="number"
-              mask="99/99/9999"
               onChange={handleChange}
               value={info.number}
-            
+            />
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label
