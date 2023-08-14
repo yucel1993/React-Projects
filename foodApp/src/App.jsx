9583292dc035f9e9d2f10a0abd7e24acf33093b0
Burhan <alpagut1993@gmail.com>
2023-08-14 15:38:42 +0200

    project :done

---
+++ foodApp/src/App.jsx
@@ -0,0 +1,22 @@
+import Category from "./components/Category"
+import Food from "./components/Food"
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <HeadlineCards/>
+     <Food/>
+     <Category/>
+    </>
+  )
+}
+
+export default App
