6c7e5d01768029eafea83966e88275b5a282525a
Burhan <alpagut1993@gmail.com>
2023-08-16 10:35:41 +0200

    newdesign 1 and 2 :added

---
+++ designWebsite2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+    <div >
+     <h1 className="text-center text-red-600">Hello Word</h1>
+    </div>
+  )
+}
+
+export default App
--- foodApp/src/components/HeadlineCards.jsx
+++ foodApp/src/components/HeadlineCards.jsx
@@ -31,7 +31,7 @@ const HeadlineCards = () => {
             <p className="px-2">Tasty Treats</p>
             <button className="border-white bg-white text-black mx-2 absolute bottom-4">Order Now</button>
             </div>
-            <img className="max-h-[160px] md:max-h-[200px] w-full object-cover object-cover rounded-xl" src="https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60" alt="" />
+            <img className="max-h-[160px] md:max-h-[200px] w-full  object-cover rounded-xl" src="https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60" alt="" />
         </div>
     </div>
   )
