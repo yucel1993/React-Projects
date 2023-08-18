c6a7e22f6943d3c316f29978895428b1202bf64f
Burhan <alpagut1993@gmail.com>
2023-08-18 19:28:26 +0200

    last:added

--- nikeLandingPage/vite.config.js
+++ nikeLandingPage/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es', // or 'cjs' for CommonJS
-      },
-    },
-  },
-}
+export default defineConfig({
+  plugins: [react()],
+})
